b3f771caa7c9450812e6f6ea720994fdfeecacd0
LyKitsune <lykuskitsune@gmail.com>
2025-12-17 16:09:19 +0800

    Base Layout done with CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+import makeTodo from "./todo_actions/makeTodo.js";
+
+import "./styles.css"
+
+
+
+
+
+/**
+ * Must be able to
+ * - Make a TODO with (title, desc, dueDate, priority)
+ * - Make a Project that has TODOs in them
+ * - View TODOs by clicking
+ * - Edit TODO
+ * - Delete TODO
+ * - Add Persistent Data using localStorage (https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API)
+ */
+
+/**
+ * Personal Notes
+ * - Each TODO can have their own ID
+ * - The ID can be used to get the appropriate TODO for editing or deleting
+ * - Return null if TODO id is invalid or DNE
+ */
---
+++ src/todo_actions/makeTodo.js
@@ -0,0 +1,10 @@
+const tasks = [];
+
+function makeTask(name, desc, dueDate, priority) {
+    this.name = name;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.id = crypto.randomUUID();
+}
+
